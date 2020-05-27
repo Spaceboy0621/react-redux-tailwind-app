3fdac7d3dd6ba616662d39ae2f3fbbd2fcf6d477
mzalevski <mzalevski.dev@gmail.com>
2020-05-27 18:51:49 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Feedback from "./routes/Feedback";
+import PersonalityTest from "./routes/PersonalityTest";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="flex-1">
+        <Navbar />
+        <Switch>
+          <Route path="/personality-test/:testId/feedback">
+            <Feedback />
+          </Route>
+          <Route path="/personality-test/:testId">
+            <PersonalityTest />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
+    </Router>
   );
 }
-
-export default App;
